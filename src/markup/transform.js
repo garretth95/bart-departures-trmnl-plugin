89cc9866168ddd9e8bbefbc74707292ce905a574
Garrett Heald <garrett.heald@stridehealth.com>
2026-02-15 21:58:26 -0800

    markup & transform improvements

--- src/markup/transform.js
+++ src/markup/transform.js
@@ -1,5 +1,34 @@
 function transform(input) {
-  const LIST_LIMIT = 8;
-  input.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit = input.ServiceDelivery.StopMonitoringDelivery.MonitoredStopVisit.slice(0, LIST_LIMIT);
-  return input;
+  const LIST_LIMIT = 25;
+
+  const delivery = input?.ServiceDelivery?.StopMonitoringDelivery;
+  if (!delivery) return input;
+
+  const d = Array.isArray(delivery) ? delivery[0] : delivery;
+
+  let visits = d?.MonitoredStopVisit || [];
+  if (!Array.isArray(visits)) visits = [visits];
+
+  const pickIso = (call) => call?.ExpectedDepartureTime || call?.ExpectedArrivalTime || null;
+
+  const departures = visits
+    .map(v => v?.MonitoredVehicleJourney)
+    .filter(j => j && j.LineRef && j.MonitoredCall?.DestinationDisplay)
+    .map(j => {
+      const call = j.MonitoredCall;
+      return {
+        dest: call.DestinationDisplay,
+        line: j.LineRef,
+        dir: j.DirectionRef || "",
+        time: pickIso(call), // ISO string
+      };
+    })
+    .filter(x => x.time) // must have a time
+    .sort((a, b) => Date.parse(a.time) - Date.parse(b.time))
+    .slice(0, LIST_LIMIT);
+
+  // return just what Liquid needs
+  return {
+    departures,
+  };
 }
